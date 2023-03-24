0e58bcdfa36bb1a6729752b95b30c42a8490c90e
Sofonias Alemayehu <87647912+HKZIYECH@users.noreply.github.com>
2023-03-24 06:48:46 -1200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,14 @@
-import tkinter as tk
+num_1 = input ("Enter First Number: ")
+num_2 = input("Enter Second Number: ")
 
-root= tk.Tk()
+op = input(("Choose an operation '+'  '-' '*' '/' "))
 
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
+if op == '+':
+    print(float(num_1) + float(num_2))
+elif op == '-':
+    print(float(num_1) - float(num_2))
+elif op == '*':
+    print(float(num_1) * float(num_2))
+elif op == '/':
+    print(float(num_1) / float(num_2))
 
-def hello ():  
-    label1 = tk.Label(root, text= 'Hello World!', fg='blue', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
-    
-button1 = tk.Button(text='Click Me', command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
-
-root.mainloop()
